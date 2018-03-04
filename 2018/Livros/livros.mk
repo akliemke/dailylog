69c7f721648380c2b031127b78f88ebbc4ad2a13
Albert Kliemke <albertkliemke@gmail.com>
2018-03-04 02:32:05 -0300

    Update books

---
+++ 2018/Livros/livros.mk
@@ -0,0 +1,3 @@
+# Livros 2018
+
+> "For a 250 page book, might spend 15 minutes on overview (discovery), 4 hours on detailed reading (understand), and 20-30 minutes making notes(remember)" - PAUL N. EDWARDS" 
